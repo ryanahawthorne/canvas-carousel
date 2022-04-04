bcf662c3aa19dd96b34771a2d2b1e40a5af62a84
JSSTV <Jonathan.Sharp@stv.tv>
2022-04-04 10:41:57 +0100

    JonathanSharpSTV | NOTICKET | draft version of react-query index and main

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ function App() {
 
     return (
         <QueryClientProvider client={queryClient}>
+            {/* components here */}
             <ReactQueryDevtools initialIsOpen />
         </QueryClientProvider>
     )
