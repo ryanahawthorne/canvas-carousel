c9216069b3764170e7b0d6a2b0e89ae39b340420
JSSTV <Jonathan.Sharp@stv.tv>
2022-04-04 10:37:02 +0100

    JonathanSharpSTV | NOTICKET | draft version of react-query index and main

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { QueryClient, QueryClientProvider, useQueryClient } from 'react-query';
+import { ReactQueryDevtools } from "react-query/devtools";
+
+const queryClient = new QueryClient();
+
+function App() {
+
+    return (
+        <QueryClientProvider client={queryClient}>
+            <ReactQueryDevtools initialIsOpen />
+        </QueryClientProvider>
+    )
+}
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(<App />, rootElement);
