4444beb5c383b7cd14c16a20e6d5c5155444467c
JSSTV <Jonathan.Sharp@stv.tv>
2022-04-04 12:22:02 +0100

    JonathanSharpSTV | NOTICKET | eslint fix problems working

--- src/index.js
+++ src/index.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { QueryClient, QueryClientProvider, useQueryClient } from 'react-query';
-import { ReactQueryDevtools } from "react-query/devtools";
+import { ReactQueryDevtools } from 'react-query/devtools';
 
 const queryClient = new QueryClient();
 
 function App() {
-
-    return (
-        <QueryClientProvider client={queryClient}>
-            {/* components here */}
-            <ReactQueryDevtools initialIsOpen />
-        </QueryClientProvider>
-    );
+  return (
+    <QueryClientProvider client={queryClient}>
+      {/* components here */}
+      <ReactQueryDevtools initialIsOpen />
+    </QueryClientProvider>
+  );
 }
 
-const rootElement = document.getElementById("root");
+const rootElement = document.getElementById('root');
 ReactDOM.render(<App />, rootElement);
