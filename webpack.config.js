7ac09db5e38b1d768891d62660b231a180502c3b
Chris Finnigan <chris.finnigan@stv.tv>
2022-04-08 14:52:22 +0100

    add eslint setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,30 +1,40 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const ESLintPlugin = require('eslint-webpack-plugin');
 
-module.exports = {
-    entry: [
-        `${__dirname}/src/index.js`
-    ],
-    output: {
-        path: `${__dirname}/dist`,
-        filename: 'index.js'
-    },
-    module: {
-        rules: [
-          {
-            test: /\.js$/,
-            exclude: /(node_modules)/,
-            use: {
-              loader: "babel-loader",
-              options: {
-                presets: ["@babel/preset-env"]
-              }
-            }
-          }
+module.exports = (env, argv) => {
+    const config = {
+        entry: [
+            `${__dirname}/src/index.js`
+        ],
+        output: {
+            path: `${__dirname}/dist`,
+            filename: 'index.js'
+        },
+        devServer: {
+            contentBase: './dist',
+            hot: true,
+        },
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /(node_modules)/,
+                    use: {
+                        loader: "babel-loader",
+                        options: {
+                            presets: ["@babel/preset-env"]
+                        }
+                    }
+                }
+            ]
+        },
+        plugins: [    
+            new HtmlWebpackPlugin({
+                template: `${__dirname}/src/index.html`
+            }),
+            new ESLintPlugin()
         ]
-    },
-    plugins: [    
-        new HtmlWebpackPlugin({
-          template: `${__dirname}/src/index.html`,
-        })
-    ]
-}
+    };
+
+    return config
+};
