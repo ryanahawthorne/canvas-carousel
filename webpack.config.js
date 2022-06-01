5b63fc9fe6db20b3736b0c3b600a4539dcb80a78
Chris Finnigan <chris.finnigan@stv.tv>
2022-06-01 12:20:48 +0100

    Webpack / Typescript (es5) setup

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/main.ts',
+  watch: true,
+  watchOptions: {
+    ignored: '**/node_modules',
+  },
+  devtool: 'inline-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
